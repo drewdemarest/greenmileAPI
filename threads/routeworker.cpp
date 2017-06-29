b22b49c45d4470d859f5eba91f8a4e740a21a49a
Unknown <drewdemarest@gmail.com>
2017-06-29 10:25:37 -0700

    ISO C++ forbids comparing pointers to true/false explicitly... fair enough.

--- threads/routeworker.cpp
+++ threads/routeworker.cpp
@@ -38,10 +38,10 @@ void RouteWorker::process() {
 
     QJsonDocument jDoc(test.exportJson());
     //QTextStream(out) stdout;
-    qDebug() << "hasPicture null? " << (false == test.getHasPicture());
+    //qDebug() << "hasPicture null? " << (false == test.getHasPicture());
     test.setHasPicture(true);
     test.setHasPicture(false);
-    qDebug() << "hasPicture null? " << (false == test.getHasPicture());
+    //qDebug() << "hasPicture null? " << (false == test.getHasPicture());
     qDebug() << QString(jDoc.toJson(QJsonDocument::Indented));
     //out << QString(jDoc.toJson(QJsonDocument::Indented));
     test.setMembersNull();
